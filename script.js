10c6ba49cb8fd68ddc6b96f57623a76034bce5da
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-04-14 21:15:14 +0200

    prove

---
+++ script.js
@@ -0,0 +1,10 @@
+//dichiaro variabili eta, km e eurokm 
+//dichiaro un prezzo base e scrivo la formula
+//if(eta<18) sconto 20%
+//else if(eta>65) sconto 40%
+//al click if(eta<18) riempi prezzoscontato
+//else if(eta>65) riempi prezzoscontato
+// else reimpi prezzobase
+//let eta = document.querySelector('.var-1');
+let eta= document.querySelector.value('.var-1');
+const eurokm = 0.21;
